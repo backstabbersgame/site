70cf6b483c6ed47ab9639eed60ee9c53c124eb79
Pedro Leite <pedro_leite@apple.com>
2023-05-29 10:19:21 -0300

    go back to iframe

--- script.js
+++ script.js
@@ -140,8 +140,28 @@ function showPage(page){
   element.item(0).classList.remove("active");
   var element = document.getElementById(page);
   element.classList.add("active");
-}
 
+  switch (page) {
+    case 'jogo':
+      document.getElementById('content').src = "jogo.html";
+      break;
+    case 'casos':
+      document.getElementById('content').src = "casos.html";
+      break;
+    case 'regras':
+      document.getElementById('content').src = "regras.html";
+      break;
+    case 'apoie':
+      document.getElementById('content').src = "apoie.html";
+      break;
+    case 'sobre':
+      document.getElementById('content').src = "sobre.html";
+      break;
+    default:
+      document.getElementById('content').src = "jogo.html";
+      break;
+  }
+};
 
 function changeLevel(){
   let nivelselect = document.getElementById('nivelselect');
